4926c9f5f6d1e3db3a3bb341aa3226cc9c48cb43
Anshul Ojha <anshulojha091@gmail.com>
2025-07-04 01:14:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
-app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
